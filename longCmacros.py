011ca8ad1254c1b93b31d4f3e9545df84563c919
Aaron Christianson <ninjaaron@gmail.com>
2019-07-05 18:42:28 +0200

    added doc strings to longCmacros.py

--- longCmacros.py
+++ longCmacros.py
@@ -7,9 +7,11 @@ ARGSEP = re.compile(r"\s*,\s*")
 W = re.compile(r"\w")
 
 
-class ReIterate:
+class ReIterator:
     def __init__(self, iterable):
-        """an iterator where values can be pushed back"""
+        """an iterator where values can be pushed back. Basically just a
+        stack you can use in a for loop.
+        """
         self.iterator = iter(iterable)
         self.stack = []
 
@@ -25,7 +27,15 @@ class ReIterate:
         return next(self.iterator)
 
 
-def bodyiter(lines):
+def bodyiter(lines: ReIterator):
+    """collect the body of a multi-line macro. The body ends as soon as
+    soon as a new preprocessor diretive is encountered (a line starting
+    with #)
+
+    A macro may also be closed explicitely with the `#enddefine`
+    directive, which will be converted into an empty line in the
+    generated code.
+    """
     for line in lines:
         if line.startswith("#enddefine"):
             lines.push("\n")
@@ -37,6 +47,14 @@ def bodyiter(lines):
 
 
 def argsub(m: re.Match):
+    """The only syntactic difference between long macros and normal
+    macros in the body is that tokens are interpolated into other names
+    without the normal ## pasting syntax. This could cause problems
+    inside of string literals.
+
+    This can be disabled with by setting `autopaste=False` when
+    calling `mkmacro`.
+    """
     body = m.string
     start = end = ""
     if m.start() != 0 and W.match(body[m.start()-1]):
@@ -46,20 +64,27 @@ def argsub(m: re.Match):
     return ''.join((start, m.group(0), end))
 
 
-def mkmacro(match: re.Match, lines: ReIterate, tabstop=8):
-    args = ARGSEP.split(match.group(1))
-    bodylines = list(bodyiter(lines))
-    prebody = ' \\\n'.join(bodylines)
-    body = re.sub('|'.join(args), argsub, prebody)
+def mkmacro(match: re.Match, bodylines, autopaste=True):
+    """Takes the regex match that looked like the beginning of a
+    multi-line macro and the lines from the body and adds trailing
+    backslashes.
+
+    By default, tokens are pasted into adjoining words. (see `argsub`
+    docstring) Dissable with `autopaste=False`.
+    """
+    body = ' \\\n'.join(bodylines)
+    if autopaste:
+        args = ARGSEP.split(match.group(1))
+        body = re.sub('|'.join(args), argsub, body)
     return "%s \\\n%s" % (match.group(0).rstrip(), body)
 
 
 def main():
-    lines = ReIterate(fileinput.input())
+    lines = ReIterator(fileinput.input())
     for line in lines:
         macromatch = MACRODEF.fullmatch(line)
         if macromatch:
-            output = mkmacro(macromatch, lines)
+            output = mkmacro(macromatch, bodyiter(lines))
             print(output)
         else:
             print(line, end="")
