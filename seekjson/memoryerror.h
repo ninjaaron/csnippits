cecdcb3505223c7bd83f1f7e08d6ea6a4b56ebdf
Aaron Christianson <ninjaaron@gmail.com>
2019-08-08 13:36:48 +0200

    added some debugging macros

--- seekjson/memoryerror.h
+++ seekjson/memoryerror.h
@@ -1,7 +1,23 @@
+#ifdef DEBUG
+
+#define debug(string) \
+	fprintf(stderr, " \x1b[31mDEBUG\x1b[0m: %s\n", string)
+#define debugf(fmtstring, ...) \
+	fprintf(stderr, " \x1b[31mDEBUG\x1b[0m:" fmtstring "\n", __VA_ARGS__)
+
+#else
+
+#define debug(string)
+#define debugf(fmtstring, ...)
+
+#endif
+
+
 #ifndef MEMORY_ERROR_H
 #define MEMORY_ERROR_H
 	
 #include <stdio.h>
+
 void memoryerror(void);
  
 #endif
